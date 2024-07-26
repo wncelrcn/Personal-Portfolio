90478f285b17d4c2a677ecda7c06cc1af4f127fd
wncelrcn <rivanowincelarcen@gmail.com>
2024-07-26 11:25:00 +0800

    Re-built the website for a more professional look, and added my new Projects and Certifications and Resume

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+// Function to change the image on hover
+    function hover(element) {
+    element.setAttribute('src', 'images/user-hover.png');
+    }
+
+    function unhover(element) {
+    element.setAttribute('src', 'images/user.png');
+    }
+    
+
